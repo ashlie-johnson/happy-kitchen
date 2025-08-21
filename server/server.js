90b4e987c06f82687c74eb75acbc57780bd17807
Dylan Stephenson <dylanstep377@gmail.com>
2025-08-21 12:18:30 -0600

    connected client with server using axios

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello from Express!');
